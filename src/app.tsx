1d2861ba8c34b9f0e1829b67ca27b72512bd3aa1
betich <betich365@gmail.com>
2021-09-12 03:47:12 +0700

    :heavy_plus_sign: add turn servers

--- src/app.tsx
+++ src/app.tsx
@@ -8,15 +8,41 @@ const db = getFirestore(firebaseApp);
 
 const servers = {
   iceServers: [
+    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun.stunprotocol.org:3478" },
+    { urls: "stun:stun.sipnet.net:3478" },
+    { urls: "stun:stun.ideasip.com:3478" },
+    { urls: "stun:stun.iptel.org:3478" },
     {
-      urls: [
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun.stunprotocol.org:3478",
-        "stun:stun.sipnet.net:3478",
-        "stun:stun.ideasip.com:3478",
-        "stun:stun.iptel.org:3478",
-      ],
+      urls: ["turn:numb.viagenie.ca"],
+      credential: "muazkh",
+      username: "webrtc@live.com",
+    },
+    {
+      urls: ["turn:192.158.29.39:3478?transport=udp"],
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      urls: ["turn:192.158.29.39:3478?transport=tcp"],
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      urls: ["turn:turn.bistri.com:80"],
+      credential: "homeo",
+      username: "homeo",
+    },
+    {
+      urls: ["turn:turn.anyfirewall.com:443?transport=tcp"],
+      credential: "webrtc",
+      username: "webrtc",
+    },
+    {
+      urls: ["turn:13.250.13.83:3478?transport=udp"],
+      username: "YzYNCouZM1mhqhmseWk6",
+      credential: "YzYNCouZM1mhqhmseWk6",
     },
   ],
   iceCandidatePoolSize: 10,
